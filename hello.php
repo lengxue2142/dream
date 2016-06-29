59e196eb742fe8b26fc893fa7a2f2b62801d3b3f
jerry <wxd314@163.com>
2016-06-29 21:48:12 +0800

    first commit test

---
+++ hello.php
@@ -0,0 +1,29 @@
+ <?php
+function myload($class){   
+      $file = './smarty/.'$class.'.class.php';   
+      if (is_file($file)) {   
+           require_once($file);   
+       }else{
+       	 exit("$file is not exists");
+       }   
+}   
+
+//函数调用方法
+spl_autoload_register('myload');   
+
+$smarty = new Smarty();
+$link = mysql_connect("localhost","root","phpmyadmin") or die('connect failed');
+ mysql_select_db("db_studentsystem") or die("select db failed");
+ mysql_query("set names utf8");
+$resData = mysql_query("select * from student");
+$user_list = array();
+
+while($row = mysql_fetch_assoc($resData)){
+   $user_list[] = $row; 
+}
+mysql_close();
+//print_r($arrDataX);
+$smarty->assign("user_list",$user_list);
+$smarty->display('index.html');
+
+?>
